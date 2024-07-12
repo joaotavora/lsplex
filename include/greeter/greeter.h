3d01970c75de85f106ac8b824e08e28c0448a0c4
João Távora <joaotavora@gmail.com>
2024-07-12 23:26:53 +0100

    fixup yakshave

--- include/greeter/greeter.h
+++ include/greeter/greeter.h
@@ -6,28 +6,28 @@
 
 namespace greeter {
 
-  /**  Language codes to be used with the Greeter class */
-  GREETER_EXPORT enum class LanguageCode { en, de, es, fr };
+/**  Language codes to be used with the Greeter class */
+GREETER_EXPORT enum class LanguageCode { en, de, es, fr };
+
+/**
+ * @brief A class for saying hello in multiple languages
+ */
+GREETER_EXPORT class Greeter {
+  std::string _name;
+
+public:
+  /**
+   * @brief Creates a new greeter
+   * @param name the name to greet
+   */
+  explicit Greeter(std::string name);
 
   /**
-   * @brief A class for saying hello in multiple languages
+   * @brief Creates a localized string containing the greeting
+   * @param lang the language to greet in
+   * @return a string containing the greeting
    */
-  GREETER_EXPORT class Greeter {
-    std::string _name;
-
-  public:
-    /**
-     * @brief Creates a new greeter
-     * @param name the name to greet
-     */
-    explicit Greeter(std::string name);
-
-    /**
-     * @brief Creates a localized string containing the greeting
-     * @param lang the language to greet in
-     * @return a string containing the greeting
-     */
-    [[nodiscard]] std::string greet(LanguageCode lang = LanguageCode::en) const;
-  };
+  [[nodiscard]] std::string greet(LanguageCode lang = LanguageCode::en) const;
+};
 
 }  // namespace greeter
