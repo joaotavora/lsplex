112669e31e0deb2359013a2d58954193c1bab495
João Távora <joaotavora@gmail.com>
2024-07-25 16:33:33 +0100

    Fix problem coming from hw4000

--- cmake/compiler-flags.cmake
+++ cmake/compiler-flags.cmake
@@ -7,7 +7,7 @@ foreach(target ${LOCAL_TARGETS})
   message(STATUS "Setting up '${target}' compiler quirks")
   if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES
                                               "GNU")
-    target_compile_options(${target} PUBLIC -Wall -Wpedantic -Wextra -Werror)
+    target_compile_options(${target} PUBLIC -Wall -Wpedantic -Wextra -Werror -Wno-sign-conversion)
   elseif(MSVC)
     target_compile_options(${target} PUBLIC /W4 /WX)
     target_compile_definitions(${target} PUBLIC DOCTEST_CONFIG_USE_STD_HEADERS)
